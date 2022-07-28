cb884734a67d278361ed170466b4aef437a7931e
heye <heye@ewell.cc>
2022-07-28 14:48:07 +0800

    .index.js

--- server/index.js
+++ server/index.js
@@ -2,22 +2,23 @@
  * @Author: heye
  * @Date: 2022-07-19 17:06:31
  * @LastEditors: heye
- * @LastEditTime: 2022-07-19 17:45:00
- * @FilePath: \Realtime-Chat-App\server\index.js
+ * @LastEditTime: 2022-07-20 11:13:13
+ * @FilePath: \REALTIME-CHAT-APP\server\index.js
  * @Description:
  *
  */
 var express = require('express')
 var cors = require('cors')
 var mongoose = require('mongoose')
+
 var app = express()
-require('dotenv').config({ path: '.env' })
+require('dotenv').config()
 
 app.use(cors())
 app.use(express.json())
 
 mongoose
-	.connect(process.env.MONGO_URL, {
+	.connect(process.env.MONGOOSE_URL, {
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
 	})
@@ -28,6 +29,6 @@ mongoose
 		console.log(err.message, 'error')
 	})
 
-app.get('/', function (req, res, next) {
-	console.log('Server start in Port 3000 successful.')
-}).listen(3000)
+const server = app.listen(process.env.PORT, () => {
+	console.log(`Server started on PORT ${process.env.PORT}`)
+})
